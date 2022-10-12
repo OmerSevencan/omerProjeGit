58e4f6aa66b6c3ecd8698808d1ade182b1546860
Omer <omer7can@gmail.com>
2022-10-12 12:50:00 +0600

    while

--- src/Gun01/Ogrenci.java
+++ src/Gun01/Ogrenci.java
@@ -1,4 +1,17 @@
 package Gun01;
 
 public class Ogrenci {
+    public static void main(String[] args) {
+        for (int i = 0 ; i < 5 ; i++){
+
+            System.out.println("merhaba");
+
+        }
+        int secim=0;
+        while (secim<5){
+            System.out.println("merhaba");
+            secim++;
+
+        }
+    }
 }
